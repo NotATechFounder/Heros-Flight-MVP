14c09456f63f3acfa4732bcad32c98e74c5b8918
igor miagkostup <igor@stansassets.com>
2023-07-18 11:13:56 +0300

    HF-15 : custom character visuals controller/upd atlasses

--- Assets/HeroesFlight/System/Character/Controllers/CharacterMovementController.cs
+++ Assets/HeroesFlight/System/Character/Controllers/CharacterMovementController.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 
 public class CharacterMovementController : MonoBehaviour
 {
-   [SerializeField] Rigidbody2D m_RigidBody;
- 
+   Rigidbody2D m_RigidBody;
+
+   void Awake()
+   {
+       m_RigidBody = GetComponent<Rigidbody2D>();
+   }
+
    public void SetVelocity(Vector3 velocity)
    {
        m_RigidBody.velocity = velocity;
---
+++ Assets/HeroesFlight/System/Character/Enum/ItemVisualType.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace HeroesFlight.System.Character.Enum
+{
+    [Serializable]
+    public enum ItemVisualType
+    {
+        Cloth,
+        Pants,
+        Wings,
+        Hat,
+        Weapon,
+        Hair,
+        Eyes,
+        Eyebrows,
+        Mouth,
+        Body
+    }
+}
