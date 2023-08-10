6447bed15bc3922fe989769806e0159ba7c6a6ec
igor miagkostup <igor@stansassets.com>
2023-08-10 11:22:46 +0300

    HF-57 : upd environment/spawn /spawn data/enemies per hit limitation

--- Assets/HeroesFlight/System/NPC/Controllers/SpawnPoint.cs
+++ Assets/HeroesFlight/System/NPC/Controllers/SpawnPoint.cs
@@ -1,17 +1,31 @@
+
 using HeroesFlightProject.System.NPC.Enum;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 namespace HeroesFlight.System.NPC.Controllers
 {
-    public class SpawnPoint : MonoBehaviour,ISpawnPointInterface
+    public class SpawnPoint : MonoBehaviour, ISpawnPointInterface
     {
-        [FormerlySerializedAs("enemyType")][SerializeField] EnemySpawmType m_EnemySpawmType;
+        [FormerlySerializedAs("enemyType")]
+        [SerializeField]
+        EnemySpawmType m_EnemySpawmType;
+
         public EnemySpawmType TargetEnemySpawmType => m_EnemySpawmType;
 
         public Vector2 GetSpawnPosition()
         {
             return transform.position;
+        }
+
+
+        void OnDrawGizmos()
+        {
+#if UNITY_EDITOR
+            var gizmosColor = m_EnemySpawmType == EnemySpawmType.Flying ? Color.red : Color.green;
+            Gizmos.color=gizmosColor;
+            Gizmos.DrawSphere(transform.position,0.2f);
+#endif
         }
     }
 }
