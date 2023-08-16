cd3a362e77bde7c7cb99db9ff78c19d2b9da083e
Oluwapelumi Ayeni <57546997+xadegamer@users.noreply.github.com>
2023-08-17 00:18:20 +0100

    Fixed merge conflit

--- Assets/HeroesFlight/System/Character/Model/CharacterSO.cs
+++ Assets/HeroesFlight/System/Character/Model/CharacterSO.cs
@@ -7,7 +7,6 @@ public class CharacterSO : ScriptableObject
 {
     [SerializeField] string characterName;
     [SerializeField] CharacterType characterType;
-    [SerializeField] HeroType heroType;
     [SerializeField] AppearanceData appearanceData;
     [SerializeField] PlayerStatData playerStatData;
     [SerializeField] AnimationData aniamtionData;
@@ -15,18 +14,10 @@ public class CharacterSO : ScriptableObject
     [SerializeField] AttackData attackData;
 
     public string CharacterName => characterName;
-    public HeroType HeroType => heroType;
     public PlayerStatData GetPlayerStatData => playerStatData;
     public AppearanceData GetAppearanceData => appearanceData;
     public AnimationData AnimationData => aniamtionData;
     public UltimateData UltimateData => ultimateData;
     public AttackData AttackData => attackData;
     public CharacterType CharacterType => characterType;
-}
-
-
-public enum HeroType
-{
-    Melee,
-    Ranged
 }
