9e816b4573cb082533ee59d38f5c362d7c547bb3
Oluwapelumi Ayeni <57546997+xadegamer@users.noreply.github.com>
2023-09-11 13:48:49 +0100

    Removed Exp noise

---
+++ Assets/HeroesFlight/Common/StatsModiifier.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsModiifier : MonoBehaviour
+{
+    
+}
+
+public struct StatModifier
+{
+    //public StatType StatType;
+    //public float Value;
+    //public StatModifierType StatModifierType;
+    //public StatModifier(StatType statType, float value, StatModifierType statModifierType)
+    //{
+    //    StatType = statType;
+    //    Value = value;
+    //    StatModifierType = statModifierType;
+    //}
+}
