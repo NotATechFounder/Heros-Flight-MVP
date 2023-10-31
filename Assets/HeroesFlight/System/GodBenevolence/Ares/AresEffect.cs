845c7cfe1aafe9d44c3fc0b38c60e94ab7bfac96
Oluwapelumi Ayeni <57546997+xadegamer@users.noreply.github.com>
2023-10-31 13:03:38 +0000

    Implementing new Ares visual

--- Assets/HeroesFlight/System/GodBenevolence/Ares/AresEffect.cs
+++ Assets/HeroesFlight/System/GodBenevolence/Ares/AresEffect.cs
@@ -8,56 +8,57 @@ using System.Collections;
 using HeroesFlight.Common.Enum;
 using HeroesFlight.System.Combat.Enum;
 using UnityEngine;
+using Spine.Unity;
+using Spine;
 
 public class AresEffect : MonoBehaviour
 {
     public Action OnHitEnemy;
 
-    [SerializeField] private float autoAttackSpeed = 2f;
+    [SerializeField] private float autoAttackInterval = 2f;
     [SerializeField] private float damage = 10f;
 
     [Header("Clash")]
-    [SerializeField] private Transform swordHolder;
-    [SerializeField] private Transform swordHandle;
+    [SerializeField] private Transform visual;
     [SerializeField] private OverlapChecker overlapChecker;
-    [SerializeField] private ParticleSystem clashEffect;
+ 
+    [Header("Animation and Viusal Settings")]
+    [SerializeField] SkeletonAnimation skeletonAnimation;
+    [SerializeField] public const string idleAnimationName = "Idle";
+    [SerializeField] public const string attackAnimation1Name = "Attack";
+    [SerializeField] public const string attack2Animation1Name = "Attack2";
 
-    [Header("Clash Foward Effect")]
-    [SerializeField] private Vector3 rotateFowardDest = new Vector3(0, 0, 360);
-    [SerializeField] private float fowardSpeed = 1f;
-    [SerializeField] private Ease fowardEase = Ease.Linear;
-
-    [Header("Clash Backward Effect")]
-    [SerializeField] private Vector3 rotateBackwardDest = new Vector3(0, 0, 180);
-    [SerializeField] private float backwardSpeed = 1f;
-    [SerializeField] private Ease backwardEase = Ease.Linear;
-
-    JuicerRuntime clashFowardEffectFoward;
-    JuicerRuntime clashFowardEffectBackward;
     private CharacterControllerInterface characterController;
     private float timer;
+    int lastAttackIndex = 0;
 
     private void Start()
     {
         overlapChecker.OnDetect += OnOverlap;
+        skeletonAnimation.AnimationState.Complete += AnimationState_Complete;
+        skeletonAnimation.AnimationState.Event += AnimationState_Event;
+    }
 
-        clashFowardEffectFoward = swordHandle.JuicyLocalRotate(rotateFowardDest, fowardSpeed);
-        clashFowardEffectFoward.SetEase(fowardEase);
-        clashFowardEffectFoward.SetOnStart(() =>
-        {
-            clashEffect.Play();
-        });
-        clashFowardEffectFoward.AddTimeEvent(0.5f, () =>
+    private void AnimationState_Event(TrackEntry trackEntry, Spine.Event e)
+    {
+        if (e.Data.Name == "Attack")
         {
             overlapChecker.Detect();
-        });
-        clashFowardEffectFoward.SetOnCompleted(() =>
-        {
-            clashFowardEffectBackward.Start();
-        });
+        }
+    }
 
-        clashFowardEffectBackward = swordHandle.JuicyLocalRotate(rotateBackwardDest, backwardSpeed);
-        clashFowardEffectBackward.SetEase(backwardEase);
+    private void AnimationState_Complete(TrackEntry trackEntry)
+    {
+        switch (trackEntry.Animation.Name)
+        {
+            case attackAnimation1Name:
+                skeletonAnimation.AnimationState.SetAnimation(0, idleAnimationName, true);
+            break;
+            case attack2Animation1Name:
+                skeletonAnimation.AnimationState.SetAnimation(0, idleAnimationName, true);
+            break;
+            default: break;
+        }
     }
 
     public IEnumerator AutoAttack()
@@ -65,12 +66,24 @@ public class AresEffect : MonoBehaviour
         while (true)
         {
             timer += Time.deltaTime;
-            if (timer >= autoAttackSpeed)
+            if (timer >= autoAttackInterval)
             {
                 timer = 0;
 
                 if(overlapChecker.TargetInRange())
-                clashFowardEffectFoward.Start();
+                {
+                    if (lastAttackIndex == 0)
+                    {
+                        skeletonAnimation.AnimationState.SetAnimation(0, attackAnimation1Name, false);
+                        lastAttackIndex = 1;
+                    }
+                    else
+                    {
+                        skeletonAnimation.AnimationState.SetAnimation(0, attack2Animation1Name, false);
+                        lastAttackIndex = 0;
+                    }
+                    overlapChecker.Detect();
+                }
             }
             yield return null;
         }
@@ -87,8 +100,8 @@ public class AresEffect : MonoBehaviour
 
     public void Flip(bool facingLeft)
     {
-        swordHolder.localScale = new Vector3(facingLeft ? 1 : -1, 1, 1);
-        overlapChecker.SetDirection(swordHolder.localScale.x == 1 ? OverlapChecker.Direction.Right : OverlapChecker.Direction.Left);
+        visual.localScale = new Vector3(facingLeft ? 1 : -1, 1, 1);
+        overlapChecker.SetDirection(visual.localScale.x == 1 ? OverlapChecker.Direction.Right : OverlapChecker.Direction.Left);
     }
 
     private void OnOverlap(int count, Collider2D[] colliders)
