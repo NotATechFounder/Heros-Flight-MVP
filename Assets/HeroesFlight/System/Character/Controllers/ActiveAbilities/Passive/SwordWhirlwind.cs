b8c0461a6627f7bfca1343ab9b668d4eed1783da
Oluwapelumi Ayeni <57546997+xadegamer@users.noreply.github.com>
2023-11-06 18:11:15 +0000

    Adding other abilities

---
+++ Assets/HeroesFlight/System/Character/Controllers/ActiveAbilities/Passive/SwordWhirlwind.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordWhirlwind : PassiveActiveAbility
+{
+    public override void OnActivated()
+    {
+
+    }
+
+    public override void OnCoolDownEnded()
+    {
+
+    }
+
+    public override void OnCoolDownStarted()
+    {
+
+    }
+}
