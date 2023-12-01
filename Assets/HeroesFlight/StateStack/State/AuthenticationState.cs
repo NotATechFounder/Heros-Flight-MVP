f083addaa23f747ed75cf046290b109ed1f5ea9d
Igor Miagkostup <igor@stansassets.com>
2023-12-01 12:26:56 +0200

    feat : separate auth state/scene

---
+++ Assets/HeroesFlight/StateStack/State/AuthenticationState.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using HeroesFlight.Core.StateStack.Enum;
+using JetBrains.Annotations;
+using StansAssets.Foundation.Patterns;
+using StansAssets.SceneManagement;
+
+
+namespace HeroesFlight.StateStack.State
+{
+    [UsedImplicitly]
+    public class AuthenticationState : BaseApplicationLoadSceneState, IAppState
+    {
+        public ApplicationState ApplicationState => ApplicationState.Authentication;
+
+        public void Init(ServiceLocator serviceLocator)
+        {
+            InitLocator(serviceLocator);
+        }
+
+        public override void ChangeState(StackChangeEvent<ApplicationState> evt, IProgressReporter progressReporter)
+        {
+            switch (evt.Action)
+            {
+                case StackAction.Added:
+
+                    var uiScene = $"{SceneType.DataScene}";
+                    m_SceneActionsQueue.AddAction(SceneActionType.Load, uiScene);
+                    m_SceneActionsQueue.Start(null, () =>
+                    {
+                        AppStateStack.State.Set(ApplicationState.Initialization);
+                    });
+                    break;
+                case StackAction.Paused:
+                    break;
+                case StackAction.Resumed:
+                    break;
+                case StackAction.Removed:
+                    progressReporter.SetDone();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(evt.Action), evt.Action, null);
+            }
+        }
+    }
+}
