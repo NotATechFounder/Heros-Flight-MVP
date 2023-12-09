a8e4ba93fb1f80f5d977bf876e860ba30f470ac1
Igor Miagkostup <igor@stansassets.com>
2023-12-09 13:39:05 +0200

    feat : resize treants/roots cleanup

--- Assets/HeroesFlight/System/NPC/Controllers/Ability/Boss/EntanglingRootsAbility.cs
+++ Assets/HeroesFlight/System/NPC/Controllers/Ability/Boss/EntanglingRootsAbility.cs
@@ -9,13 +9,19 @@ using UnityEngine;
 
 namespace HeroesFlight.System.NPC.Controllers.Ability
 {
+    /// <summary>
+    /// Represents the ability of a boss to entangle its targets with roots.
+    /// </summary>
     public class EntanglingRootsAbility : BossAbilityBase
     {
         [SerializeField] GameObject entityHolder;
         [SerializeField] float logicTriggerDelay = 2f;
-        [Header("Parameters for optional visual zone usage")] 
-        [SerializeField] WarningLine zone;
+
+        [Header("Parameters for optional visual zone usage")] [SerializeField]
+        WarningLine zone;
+
         [SerializeField] float zoneWidth;
+
         [Header("Effect To Apply on Target")] [SerializeField]
         RootStatusEffect rootEffect;
 
@@ -35,58 +41,90 @@ namespace HeroesFlight.System.NPC.Controllers.Ability
             }
         }
 
+        /// <summary>
+        /// Handles the detection of targets and applies the root effect to each target. </summary> <param name="count">The number of targets detected.</param> <param name="targets">An array of Collider2D objects representing the targets.</param>
+        /// /
         void HandleTargetsDetected(int count, Collider2D[] targets)
         {
             for (int i = 0; i < count; i++)
             {
-                if (targets[i].TryGetComponent<CombatEffectsController>(out var controller))
-                {
-                    controller.ApplyStatusEffect(rootEffect, 1);
-                }
+                TryApplyRootEffect(targets[i]);
+            }
+        }
+
+        /// <summary>
+        /// Tries to apply a root effect to the specified target collider.
+        /// </summary>
+        /// <param name="target">The collider to apply the effect to.</param>
+        void TryApplyRootEffect(Collider2D target)
+        {
+            if (target.TryGetComponent<CombatEffectsController>(out var controller))
+            {
+                controller.ApplyStatusEffect(rootEffect, 1);
             }
         }
 
 
+        /// <summary>
+        /// Uses the ability of the entity.
+        /// </summary>
+        /// <param name="onComplete">The action to be executed when the ability is complete. Default is null.</param>
         public override void UseAbility(Action onComplete = null)
         {
             base.UseAbility(onComplete);
             runtimeEntityHolder.transform.position = transform.position;
-            foreach (var mushroom in mushrooms)
-            {
-                mushroom.ToggleIndicator(true);
-            }
+
+            ToggleAllMushroomIndicators(true);
 
             if (zone == null)
             {
-                CoroutineUtility.WaitForSeconds(logicTriggerDelay, () =>
-                {
-                    cameraShaker?.ShakeCamera(CinemachineImpulseDefinition.ImpulseShapes.Explosion, .5f);
-                    foreach (var mushroom in mushrooms)
-                    {
-                        mushroom.StartDetection();
-                    }
-                });
+                CoroutineUtility.WaitForSeconds(logicTriggerDelay, TriggerMushroomDetectionAndCameraShake);
             }
             else
             {
-                zone.Trigger(() =>
-                {
-                    cameraShaker?.ShakeCamera(CinemachineImpulseDefinition.ImpulseShapes.Explosion, .5f);
-                    foreach (var mushroom in mushrooms)
-                    {
-                        mushroom.StartDetection();
-                    }
-                }, logicTriggerDelay, zoneWidth);
+                zone.Trigger(TriggerMushroomDetectionAndCameraShake, logicTriggerDelay, zoneWidth);
+            }
+        }
+
+        private void ToggleAllMushroomIndicators(bool state)
+        {
+            foreach (var mushroom in mushrooms)
+            {
+                mushroom.ToggleIndicator(state);
+            }
+        }
+
+        /// <summary>
+        /// Triggers the mushroom detection event and shakes the camera.
+        /// </summary>
+        private void TriggerMushroomDetectionAndCameraShake()
+        {
+            cameraShaker?.ShakeCamera(CinemachineImpulseDefinition.ImpulseShapes.Explosion, .5f);
+
+            foreach (var mushroom in mushrooms)
+            {
+                mushroom.StartDetection();
             }
         }
 
 
+        /// <summary>
+        /// Stops the ability of the character.
+        /// </summary>
         public override void StopAbility()
         {
             foreach (var mushroom in mushrooms)
             {
                 mushroom.gameObject.SetActive(false);
             }
+        }
+
+        /// <summary>
+        /// This method is called when the component or object is being destroyed.
+        /// </summary>
+        private void OnDestroy()
+        {
+            Destroy(runtimeEntityHolder);
         }
     }
 }
