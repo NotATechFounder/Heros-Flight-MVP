61d4747551bad0e03132ab8213e50868b1ab9e79
Oluwapelumi Ayeni <57546997+xadegamer@users.noreply.github.com>
2023-12-16 13:01:37 +0000

    Preparing tutorial core

---
+++ Assets/HeroesFlight/System/UI/Controllers/GameButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UISystem
+{
+    [Serializable]
+    public class GameButton
+    {
+        public enum GameButtonType
+        {
+            Mainmenu_Play, Mainmenu_Pause,
+            GameMenu_Pause
+        }
+
+        public enum GameButtonVisiblity
+        {
+            Visible,
+            Hidden
+        }
+
+        public GameButtonType type;
+        public AdvanceButton advanceButton;
+        private GameButtonVisiblity currentState;
+
+        public GameButtonVisiblity GetVisiblity => currentState;
+        public void ToggleVisibility(GameButtonVisiblity state)
+        {
+            currentState = state;
+            advanceButton.interactable = state == GameButtonVisiblity.Visible;
+        }
+    }
+}
