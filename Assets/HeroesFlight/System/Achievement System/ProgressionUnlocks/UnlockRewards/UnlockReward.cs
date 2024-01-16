be2e2517ffa5ed277fc59fd837f3023e67f78ae3
Igor Miagkostup <igor@stansassets.com>
2024-01-16 12:31:23 +0200

    AHF-339 : main menu locking

--- Assets/HeroesFlight/System/Achievement System/ProgressionUnlocks/UnlockRewards/UnlockReward.cs
+++ Assets/HeroesFlight/System/Achievement System/ProgressionUnlocks/UnlockRewards/UnlockReward.cs
@@ -5,6 +5,8 @@ namespace HeroesFlight.System.Achievement_System.ProgressionUnlocks.UnlockReward
     public abstract class UnlockReward : ScriptableObject
     {
         [SerializeField] private ProgressionUnlockType unlockType;
+        [SerializeField] private Sprite rewardImage;
         public ProgressionUnlockType UnlockType => unlockType;
+        public Sprite RewardImage => rewardImage;
     }
 }
