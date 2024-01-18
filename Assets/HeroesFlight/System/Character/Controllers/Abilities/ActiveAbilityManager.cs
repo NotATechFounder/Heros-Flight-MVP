a61e5aaa2644a8bbe668d8efd1eb460563811707
Oluwapelumi Ayeni <57546997+xadegamer@users.noreply.github.com>
2024-01-18 12:35:19 +0000

    improved

--- Assets/HeroesFlight/System/Character/Controllers/Abilities/ActiveAbilityManager.cs
+++ Assets/HeroesFlight/System/Character/Controllers/Abilities/ActiveAbilityManager.cs
@@ -560,8 +560,8 @@ public class ActiveAbilityManager : MonoBehaviour
 
     public void ResetAbility()
     {
-        // eqquipedPassiveAbilities.Clear();
-        // eqquipedActiveActivities.Clear();
+        eqquipedPassiveAbilities.Clear();
+        eqquipedActiveActivities.Clear();
     }
 
     private void OnValidate()
