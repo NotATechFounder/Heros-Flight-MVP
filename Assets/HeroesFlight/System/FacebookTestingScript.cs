fcdc321328fcbf0bdb6abb73e4e14d4c9dc60d7c
Anthony Ngo <anthony@studiogema.io>
2024-02-07 22:56:54 +1100

    Facebook SDK

---
+++ Assets/HeroesFlight/System/FacebookTestingScript.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+using Facebook.Unity;
+
+
+public class Analytics : MonoBehaviour
+{
+    void Awake()
+    {
+        if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            Application.targetFrameRate = 60;
+        }
+
+        FB.Init(FBInitCallback);
+    }
+
+    private void FBInitCallback()
+    {
+        if (FB.IsInitialized)
+        {
+            FB.ActivateApp();
+        }
+    }
+
+    public void OnApplicationPause(bool paused)
+    {
+        if (!paused)
+        {
+            if (FB.IsInitialized)
+            {
+                FB.ActivateApp();
+            }
+        }
+    }
+}
