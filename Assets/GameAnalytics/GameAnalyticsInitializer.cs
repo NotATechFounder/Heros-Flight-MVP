810b66c9d161647e56634bef02b65ded631f5d5e
Igor Miagkostup <igor@stansassets.com>
2024-02-23 18:06:49 +0200

    fix : init skd/move gameanalytics to bootstrap/meta files

---
+++ Assets/GameAnalytics/GameAnalyticsInitializer.cs
@@ -0,0 +1,65 @@
+﻿using GameAnalyticsSDK;
+using UnityEngine;
+
+namespace HeroesFlight.GameAnalyticsHandler
+{
+    public class GameAnalyticsInitializer : MonoBehaviour, IGameAnalyticsATTListener
+    {
+        private void Awake()
+        {
+            if(Application.platform == RuntimePlatform.IPhonePlayer)
+            {
+                GameAnalytics.RequestTrackingAuthorization(this);
+            }
+            else
+            {
+                GameAnalytics.Initialize();
+            }
+        }
+
+        /// Method: GameAnalyticsATTListenerNotDetermined
+        /// Description: This method is used to handle the game analytics ATT listener when authorization status is not determined.
+        /// Parameters: None
+        /// Returns: None
+        /// /
+        public void GameAnalyticsATTListenerNotDetermined()
+        {
+            GameAnalytics.Initialize();
+        }
+
+        /// <summary>
+        /// This method is used as a listener for the restricted version of GameAnalytics' AppTrackingTransparency (ATT) feature.
+        /// It is responsible for initializing the GameAnalytics SDK.
+        /// </summary>
+        /// <remarks>
+        /// The ATT feature aims to collect users' permissions related to app tracking on iOS devices.
+        /// This method initializes the GameAnalytics SDK, which includes any necessary setup or configuration steps.
+        /// </remarks>
+        public void GameAnalyticsATTListenerRestricted()
+        {
+            GameAnalytics.Initialize();
+        }
+
+        /// <summary>
+        /// This method is used to handle the event when the user denies App Tracking Transparency (ATT) permission.
+        /// It initializes the GameAnalytics instance.
+        /// </summary>
+        public void GameAnalyticsATTListenerDenied()
+        {
+            GameAnalytics.Initialize();
+        }
+
+        /// <summary>
+        /// This method is an event listener for when the user's consent for data tracking is authorized.
+        /// It initializes the GameAnalytics SDK.
+        /// </summary>
+        /// <remarks>
+        /// Call this method when the user has authorized the consent for data tracking.
+        /// This method ensures that the GameAnalytics SDK is properly initialized and ready to track data.
+        /// </remarks>
+        public void GameAnalyticsATTListenerAuthorized()
+        {
+            GameAnalytics.Initialize();
+        }
+    }
+}
